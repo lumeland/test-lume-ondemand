6d5c5b68d5030b98da120726b039a3621870243f
Oscar Otero <oom@oscarotero.com>
2021-10-30 20:47:42 +0200

    log

--- server.ts
+++ server.ts
@@ -2,9 +2,10 @@ import site from "./_config.ts";
 import { listenAndServe } from "https://deno.land/std@0.111.0/http/server.ts";
 
 async function handler(req: Request) {
+  console.log(req);
   const response = await site.onDemand.response(new URL(req.url));
   return response || new Response("Not found", { status: 404 });
 }
-console.log(site.options);
+console.log(Deno.readTextFileSync(Deno.cwd() + "/_ondemand.json"));
 console.log("Listening on http://localhost:8000");
 await listenAndServe(":8000", handler);

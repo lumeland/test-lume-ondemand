de825938aa0544508eed2204a529c195aa2683df
Oscar Otero <oom@oscarotero.com>
2021-10-30 20:58:27 +0200

    test

--- _config.ts
+++ _config.ts
@@ -1,4 +1,4 @@
-import lume from "https://raw.githubusercontent.com/lumeland/lume/v2.x-dev/mod.ts";
+import lume from "https://raw.githubusercontent.com/lumeland/lume/2777e91598bb2da254d140840461020ead10ea95/mod.ts";
 
 const site = lume({}, {}, false);
 

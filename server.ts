d17e92b2c30ad100a9ab487d9b3c7dc45deae129
Oscar Otero <oom@oscarotero.com>
2021-10-31 10:25:09 +0100

    test

--- server.ts
+++ server.ts
@@ -2,11 +2,7 @@ import site from "./_config.ts";
 import { listenAndServe } from "https://deno.land/std@0.111.0/http/server.ts";
 
 async function handler(req: Request) {
-  console.log(req.url);
   const response = await site.onDemand.response(new URL(req.url));
-  console.log("root", site.source.root?.pages);
-  site.source.reload("/example.md");
-  console.log("root after reload", site.source.root?.pages);
   return response || new Response("Not found", { status: 404 });
 }
 console.log("Listening on http://localhost:8000");

7f3cf0c868ea48b5ea914a55d23f8e1ac6743d5a
Oscar Otero <oom@oscarotero.com>
2021-11-01 17:26:54 +0100

    try to debug on_demand

--- server.ts
+++ server.ts
@@ -5,12 +5,15 @@ import { listenAndServe } from "https://deno.land/std@0.113.0/http/server.ts";
 async function handler(request: Request) {
   if (site.options.server.router) {
     const url = new URL(request.url);
-    const result = await site.options.server.router(url);
-
-    if (result) {
-      // const [body, response] = result;
-      // console.log({body, response});
-      // return new Response(body, response);
+    try {
+      const result = await site.options.server.router(url);
+  
+      if (result) {
+        const [body, response] = result;
+        return new Response(body, response);
+      }
+    } catch (error) {
+      console.error(error);
     }
   }
 

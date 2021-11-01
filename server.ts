ddde5bb63dad20b0cf1c3a481fb6b9c7d1216752
Oscar Otero <oom@oscarotero.com>
2021-11-01 13:24:29 +0100

    log error

--- server.ts
+++ server.ts
@@ -3,10 +3,10 @@ import { listenAndServe } from "https://deno.land/std@0.111.0/http/server.ts";
 
 async function handler(req: Request) {
   const result = await site.onDemand.response(new URL(req.url));
-
+console.log(result);
   if (result) {
     const [body, response] = result;
-    return new Response(body, response);
+    // return new Response(body, response);
   }
 
   return new Response("Not found", { status: 404 });

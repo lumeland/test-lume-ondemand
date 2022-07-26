0a25fee7ca6335cfecbe86b279296007132486ce
Oscar Otero <oom@oscarotero.com>
2022-07-27 01:52:34 +0200

    added datetime

--- _config.ts
+++ _config.ts
@@ -1,9 +1,11 @@
 import lume from "lume/mod.ts";
+import date from "lume/plugins/date.ts";
 import onDemand from "lume/plugins/on_demand.ts";
 
 const site = lume({}, {}, false);
 
 site.use(onDemand());
+site.use(date());
 site.ignore("README.md");
 
 export default site;

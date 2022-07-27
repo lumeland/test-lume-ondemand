fbc1aac9f166d3ade590726baaae4f97531aafa0
Oscar Otero <oom@oscarotero.com>
2022-07-27 02:13:08 +0200

    fixed content type

--- serve.ts
+++ serve.ts
@@ -10,6 +10,18 @@ const server = new Server({
 
 const router = new JsonRouter(site.src("_routes.json"));
 
+// Todo: Fix this
+server.use(async (request, next) => {
+  const response = await next(request);
+  const type = response.headers.get("content-type");
+
+  if (!type || type === "text/plain;charset=UTF-8") {
+    response.headers.set("content-type", "text/html;charset=UTF-8");
+  }
+
+  return response;
+});
+
 server.use(onDemand({
   site,
   router: (url) => router.match(url),
